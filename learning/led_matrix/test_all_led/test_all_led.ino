ad7390d77e5f1b4058f98ea1efdd2a6d620e2cee
GamerDra <all4gaming.29@gmail.com>
2025-01-11 19:57:17 +0530

    learning progress for led matrix

--- learning/led_matrix/test_all_led/test_all_led.ino
+++ learning/led_matrix/test_all_led/test_all_led.ino
@@ -1,9 +1,9 @@
 // Define row and column pin mappings
 int rows[] = {21, 22, 23, 25, 26, 27, 32, 33}; // Row pins
-int cols[] = {4, 5, 13, 14, 16, 17, 18, 19};   // Column pins
+int cols[] = {4, 5, 13, 14, 16, 17, 18, 19};   // Column pins refer to the readme for connections
 
 void setup() {
-  // Set all row pins as OUTPUT
+  
   for (int i = 0; i < 8; i++) {
     pinMode(rows[i], OUTPUT);
     digitalWrite(rows[i], LOW); // Turn rows off initially
@@ -26,7 +26,7 @@ void loop() {
   }
 }
 
-// Function to light up a single LED at (rowIdx, colIdx)
+
 void lightLED(int rowIdx, int colIdx) {
   // Turn off all rows and columns first
   for (int i = 0; i < 8; i++) {
